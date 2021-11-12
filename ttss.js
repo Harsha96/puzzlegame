077c106d529b8761603732827fb6103a21114ba7
Harsha Hirushan <hirushanharsha4@gmail.com>
2021-11-12 15:39:06 +0530

    Add files via upload

---
+++ ttss.js
@@ -0,0 +1,108 @@
+var ttss = [
+	{
+		"word":"Sirius",
+		"clue":"What is the brightest star in the sky?"
+	},
+	{
+		"word":"AstronomicalTelescope",
+		"clue":"What is the equipment that we used to watch stars from earth?"
+	},
+	{
+		"word":" Karmanline",
+		"clue":"Where does space begin?"
+	},
+	{
+		"word":"scatter",
+		"clue":"Why is the sky blue?"
+	},
+	{
+		"word":"ValentinaTereshkova",
+		"clue":"Who is the first Woman went to space?"
+	},
+	{
+		"word":"Sputnik1",
+		"clue":"What is the first spacecraft?"
+	},
+	{
+		"word":"NeilArmstrong",
+		"clue":"Whom was the first man went onto the space"
+	},
+	{
+		"word":"Jupiter",
+		"clue":"What is the biggest planet in the solar system?"
+	},
+	{
+		"word":"Hydronium",
+		"clue":"what is the element which is Not in the Sun?"
+	},
+	{
+		"word":"Jupiter",
+		"clue":"The largest circular storm in our solar system is on the surface of which of the following planets?"
+	},
+	{
+		"word":"Solarwind",
+		"clue":"The rapidly moving stream of charged particles that is being driven away from the sun is known as what?"
+	},
+	{
+		"word":"Ceres",
+		"clue":"The biggest asteroid known is:"
+	},
+	{
+		"word":"Challenger",
+		"clue":"What is the Space Shuttle that destroyed?"
+	},
+	{
+		"word":"Proximacentauri",
+		"clue":"What is the nearest star for Earth?"
+	},
+	{
+		"word":"vacuum",
+		"clue":"The space is a ____"
+	},
+	{
+		"word":"DennisTito",
+		"clue":"Who is the first space tourist?" 
+	},
+	{
+		"word":"Cepheid",
+		"clue":"What is the name of the star that pilates radially,changing both diameter and temperature?"
+	},
+	{
+		"word":"Syzygy",
+		"clue":"What term describes the alignment of three celestial bodies?"
+	},
+	{
+		"word":"Quasar",
+		"clue":"The name of the radio source that is very far from the earth?"
+	},
+	{
+		"word":"Sojourner",
+		"clue":"Which rover is the first to explorer to mars?"
+	},
+	{
+		"word":"NeutronStar",
+		"clue":"The most dense substance in the universe?"
+	},
+	{
+		"word":"Apophis",
+		"clue":"Which asteroid will approach dangerously to  the earth in 2029 and 2035?"
+	},
+	{
+		"word":"Luminosity",
+		"clue":"The amount of tye light emitted by an object in a unit of time is known as?"
+	},
+	{
+		"word":"Scintillation",
+		"clue":"The scientific term used for twinkling of stars?"
+	},
+
+	{
+		"word":"Lapetus",
+		"clue":"Which of starn’s moons has such distinctive equatorial ridge?"
+	},
+	{
+		"word":"UrsaMajor",
+		"clue":"What constellation is the big dipper?"
+	},
+	
+]
